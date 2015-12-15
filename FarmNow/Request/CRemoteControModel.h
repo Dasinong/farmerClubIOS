47565d882315726f6ccd2658dac63c20cce5ba92
zheliang <865764025@qq.com>
2015-12-15 21:46:41 +0800

    修改配置到公司账户 增加远程控制qq wx登录是否显示

---
+++ FarmNow/Request/CRemoteControModel.h
@@ -0,0 +1,13 @@
+//
+//  CRemoteControModel.h
+//  FarmNow
+//
+//  Created by zheliang on 15/12/15.
+//  Copyright © 2015年 zheliang. All rights reserved.
+//
+
+#import "CResponseBase.h"
+
+@interface CRemoteControModel : CResponseModel
+
+@end
---
+++ FarmNow/Request/CRemoteControModel.m
@@ -0,0 +1,13 @@
+//
+//  CRemoteControModel.m
+//  FarmNow
+//
+//  Created by zheliang on 15/12/15.
+//  Copyright © 2015年 zheliang. All rights reserved.
+//
+
+#import "CRemoteControModel.h"
+
+@implementation CRemoteControModel
+
+@end
