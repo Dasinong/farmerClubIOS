caabee645fab5ebfe6f186036f35eb67d35a33cd
ZhuXiChi <zhuxichi@shaozi.info>
2016-03-08 17:00:44 +0800

    店铺的巴斯夫首页

--- FarmNow/UI/CBASFNaviViewController.m
+++ FarmNow/UI/CBASFNaviViewController.m
@@ -8,8 +8,25 @@
 
 #import "CBASFNaviViewController.h"
 #import "AppDelegate.h"
+#import "CPersonalCache.h"
+
+@interface CBASFNaviViewController ()
+@property (nonatomic, weak) IBOutlet UIButton *qrButton;
+@end
 
 @implementation CBASFNaviViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if ([[USER.userType lowercaseString] isEqualToString:@"retailer"]) {
+        [self.qrButton setImage:[UIImage image_s:@"BASFNaviButton4Alt"] forState:UIControlStateNormal];
+    }
+    else {
+        [self.qrButton setImage:[UIImage image_s:@"BASFNaviButton4"] forState:UIControlStateNormal];
+    }
+    
+}
+
 - (IBAction)weather:(id)sender {
     AppDelegate *delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
     UIViewController* controller = [self.storyboard instantiateViewControllerWithIdentifier:@"CWeatherDrawerController"];
